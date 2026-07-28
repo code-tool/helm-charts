2dcac9b20fed593d1b06f109e7df7ceadeae3d14
andrycorr <andrycorry@gmail.com>
2026-07-28 13:29:58 +0300

    add authentik-outpost helm chart (#7)
    * add authentik-outpost helm chart

---
+++ charts/authentik-outpost/tests/golden/unit/default_test.go
@@ -0,0 +1,32 @@
+package golden
+
+import (
+	"path/filepath"
+	"testing"
+	"tests/golden"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestGoldenDefault(t *testing.T) {
+	t.Parallel()
+
+	chartPath, err := filepath.Abs("../../..")
+	require.NoError(t, err)
+
+	suite.Run(t, &golden.TemplateGoldenTest{
+		ChartPath:      chartPath,
+		Release:        "golden-file-test",
+		Namespace:      "test-namespace",
+		GoldenFileName: "default",
+		Templates: []string{
+			"templates/deployment.yaml",
+			"templates/secret.yaml",
+			"templates/service.yaml",
+		},
+		ValuesFiles: []string{
+			"../../values/default.values.yaml",
+		},
+	})
+}
